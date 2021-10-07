035137165dae33d0838a1cdb80619eee368f1956
McKenna Tey <mckenna.tey@gmail.com>
2021-10-06 21:52:12 -0700

    likes assingments

---
+++ WK6/Likes Assginment/action.js
@@ -0,0 +1,21 @@
+
+let numlikes= 0
+let countnum =document.querySelector("#countnum")
+function addlikes() {
+    numlikes++
+    countnum.innerText = numlikes
+}
+
+let likelikes= 0
+let taylikes =document.querySelector("#taylikes")
+function pluslikes() {
+    likelikes++
+    taylikes.innerText = likelikes
+}
+
+let manylikes= 0
+let brittlikes =document.querySelector("#brittlikes")
+function addinglikes() {
+    manylikes++
+    brittlikes.innerText = manylikes
+}
